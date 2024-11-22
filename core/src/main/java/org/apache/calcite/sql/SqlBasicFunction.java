6274092992b1f5fe8d5949fcc6e58d0cb0a3b79e
TJ Banghart <tjbanghart@gmail.com>
2024-11-22 13:50:40 -0800

    [CALCITE-6688] Allow basic symmetric operators to be reversed.

--- core/src/main/java/org/apache/calcite/sql/SqlBasicFunction.java
+++ core/src/main/java/org/apache/calcite/sql/SqlBasicFunction.java
@@ -158,6 +158,10 @@ public final class SqlBasicFunction extends SqlFunction {
   @Override public boolean isDeterministic() {
     return deterministic;
   }
+  
+  @Override public @Nullable SqlOperator reverse() {
+    return isSymmetrical() ? this : null;
+  }
 
   @Override public SqlMonotonicity getMonotonicity(SqlOperatorBinding call) {
     return monotonicityInference.apply(call);
